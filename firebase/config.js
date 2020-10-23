c589ff44d403931b2379211bce4cf3d0a7239530
Lauren Lazzaro <laurenmlazzaro@gmail.com>
2020-10-23 01:05:24 -0500

    Set up Firebase

---
+++ firebase/config.js
@@ -0,0 +1,10 @@
+export default {
+  apiKey: process.env.FIREBASE_API,
+  authDomain: "ashlynns-arrangements-admin-db.firebaseapp.com",
+  databaseURL: "https://ashlynns-arrangements-admin-db.firebaseio.com",
+  projectId: "ashlynns-arrangements-admin-db",
+  storageBucket: "ashlynns-arrangements-admin-db.appspot.com",
+  messagingSenderId: "381803912491",
+  appId: process.env.FIREBASE_ID,
+  measurementId: "G-1N6G5KWPP6"
+}
