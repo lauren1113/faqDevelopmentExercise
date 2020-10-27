0a0c6af51a28301f6a3c0369634db59e6a5a428f
Lauren Lazzaro <laurenmlazzaro@gmail.com>
2020-10-26 19:57:00 -0500

    Save HTML

--- components/Nav.js
+++ components/Nav.js
@@ -3,7 +3,12 @@ export default links => `
     <div id="navBarContainer">
       <h2 id="nav-title">Ashlynn's Arrangements</h2>
       <div id="navButtonsContainer">
-      ${links.map(link => `<button class="navButton"><a href="/${link.title}" data-navigo>${link.text}</a></button>`).join("")}
+      ${links
+        .map(
+          link =>
+            `<button class="navButton"><a href="/${link.title}" data-navigo>${link.text}</a></button>`
+        )
+        .join("")}
       </div>
     </div>
   </nav>
--- index.js
+++ index.js
@@ -1,10 +1,20 @@
-import { Header, Logo, pageTitle, Main, Footer } from "./components";
+import { Nav, Header, Main, Footer } from "./components";
 import * as state from "./store";
 import Navigo from "navigo";
 import { capitalize } from "lodash";
 import axios from "axios";
 import "./env";
 import { auth, db } from "./firebase";
+
+axios
+  .get(`https://api.github.com/users/lauren1113/repos`, {
+    headers: {
+      Authorization: `token ${process.env.GITHUB_TOKEN}`
+    }
+  })
+  .then(response => console.log(response.data));
+
+const router = new Navigo(window.location.origin);
 
 router
   .on(":view", handleRoute)
@@ -19,9 +29,8 @@ router.updatePageLinks();
 
 function render(st) {
   document.querySelector("#root").innerHTML = `
-    ${Header(st)}
-    ${Logo()}
-    ${pageTitle(st)}
+    ${Nav(st)}
+    ${Header()}
     ${Main(st)}
     ${Footer()}
   `;
@@ -30,39 +39,123 @@ function render(st) {
   firebase.initializeApp(firebaseConfig);
   firebase.analytics();
 
+  // [FIREBASE USER DATABASE]
 
-// [FIREBASE USER DATABASE]
+  function addSignInListeners(st) {
+    addLogInAndOutListener(state.User);
+    listenForAuthChange();
+    listenForRegister(st);
+    listenForSignIn(st);
+  }
 
-function addSignInListeners(st) {
-  addLogInAndOutListener(state.User);
-  listenForAuthChange();
-  listenForRegister(st);
-  listenForSignIn(st);
-}
+  // FUNCTIONS & EVENT LISTENERS
+  function addLogInAndOutListener(user) {
+    // select link in header
+    document.querySelector("header a").addEventListener("click", event => {
+      // if user is logged in,
+      if (user.loggedIn) {
+        event.preventDefault();
+        // log out functionality
+        auth.signOut().then(() => {
+          logOutUserInDb(user.email);
+          resetUserInState();
+          // update user in database
+          db.collection("Users").get;
+          render(state.Home);
+          router.navigate("/Home");
+        });
+      }
+      // if user is logged out, clicking the link will route to sign in page
+    });
+  }
+  function logOutUserInDb(email) {
+    if (state.User.loggedIn) {
+      db.collection("Users")
+        .get()
+        .then(snapshot =>
+          snapshot.docs.forEach(doc => {
+            if (email === doc.data().email) {
+              let id = doc.id;
+              db.collection("Users")
+                .doc(id)
+                .update({ signedIn: false });
+            }
+          })
+        );
+    }
+  }
+  function resetUserInState() {
+    state.User.username = "";
+    state.User.firstName = "";
+    state.User.lastName = "";
+    state.User.email = "";
+    state.User.loggedIn = false;
+  }
 
-// FUNCTIONS & EVENT LISTENERS
-function addLogInAndOutListener(user) {
-  // select link in header
-  document.querySelector("header a").addEventListener("click", event => {
-    // if user is logged in,
-    if (user.loggedIn) {
+  function listenForAuthChange() {
+    // log user object from auth if a user is signed in
+    auth.onAuthStateChanged(user => (user ? console.log(user) : ""));
+  }
+
+  function listenForRegister(st) {
+    document.querySelector("#newUserForm").addEventListener("submit", event => {
       event.preventDefault();
-      // log out functionality
-      auth.signOut().then(() => {
-        logOutUserInDb(user.email);
-        resetUserInState();
-        // update user in database
-        db.collection("Users").get;
+      // convert HTML elements to Array
+      let inputList = Array.from(event.target.elements);
+      // remove submit button from list
+      inputList.pop();
+      const inputs = inputList.map(input => input.value);
+      let firstName = inputs[0];
+      let lastName = inputs[1];
+      let email = inputs[2];
+      let password = inputs[3];
+
+      // create user in Firebase
+      auth.createUserWithEmailAndPassword(email, password).then(response => {
+        addUserToStateAndDb(firstName, lastName, email, password);
         render(state.Home);
         router.navigate("/Home");
       });
-    }
-    // if user is logged out, clicking the link will route to sign in page
-  });
-}
-function logOutUserInDb(email) {
-  if (state.User.loggedIn) {
-    db.collection("Users")
+    });
+  }
+
+  function addUserToStateAndDb(first, last, email, pass) {
+    state.User.username = first + last;
+    state.User.firstName = first;
+    state.User.lastName = last;
+    state.User.email = email;
+    state.User.loggedIn = true;
+
+    db.collection("Users").add({
+      firstName: first,
+      lastName: last,
+      email: email,
+      password: pass,
+      signedIn: true
+    });
+  }
+
+  function listenForSignIn(st) {
+    document
+      .querySelector("#existingUserForm")
+      .addEventListener("submit", event => {
+        event.preventDefault();
+        // convert HTML elements to Array
+        let inputList = Array.from(event.target.elements);
+        // remove submit button from list
+        inputList.pop();
+        const inputs = inputList.map(input => input.value);
+        let email = inputs[0];
+        let password = inputs[1];
+        auth.signInWithEmailAndPassword(email, password).then(() => {
+          getUserFromDb(email).then(() => render(state.Home));
+          router.navigate("/Home");
+        });
+      });
+  }
+  function getUserFromDb(email) {
+    return db
+      .collection("Users")
       .get()
       .then(snapshot =>
         snapshot.docs.forEach(doc => {
@@ -70,97 +163,15 @@ function logOutUserInDb(email) {
             let id = doc.id;
             db.collection("Users")
               .doc(id)
-              .update({ signedIn: false });
+              .update({ signedIn: true });
+            let user = doc.data();
+            state.User.username = user.firstName + user.lastName;
+            state.User.firstName = user.firstName;
+            state.User.lastName = user.lastName;
+            state.User.email = email;
+            state.User.loggedIn = true;
           }
         })
       );
   }
-}
-function resetUserInState() {
-  state.User.username = "";
-  state.User.firstName = "";
-  state.User.lastName = "";
-  state.User.email = "";
-  state.User.loggedIn = false;
-}
-
-function listenForAuthChange() {
-  // log user object from auth if a user is signed in
-  auth.onAuthStateChanged(user => (user ? console.log(user) : ""));
-}
-
-function listenForRegister(st) {
-  document.querySelector("#newUserForm").addEventListener("submit", event => {
-    event.preventDefault();
-    // convert HTML elements to Array
-    let inputList = Array.from(event.target.elements);
-    // remove submit button from list
-    inputList.pop();
-    const inputs = inputList.map(input => input.value);
-    let firstName = inputs[0];
-    let lastName = inputs[1];
-    let email = inputs[2];
-    let password = inputs[3];
-
-    // create user in Firebase
-    auth.createUserWithEmailAndPassword(email, password).then(response => {
-      addUserToStateAndDb(firstName, lastName, email, password);
-      render(state.Home);
-      router.navigate("/Home");
-    });
-  });
-}
-
-function addUserToStateAndDb(first, last, email, pass) {
-  state.User.username = first + last;
-  state.User.firstName = first;
-  state.User.lastName = last;
-  state.User.email = email;
-  state.User.loggedIn = true;
-
-  db.collection("Users").add({
-    firstName: first,
-    lastName: last,
-    email: email,
-    password: pass,
-    signedIn: true
-  });
-}
-
-function listenForSignIn(st) {
-  document.querySelector("#existingUserForm").addEventListener("submit", event => {
-    event.preventDefault();
-    // convert HTML elements to Array
-    let inputList = Array.from(event.target.elements);
-    // remove submit button from list
-    inputList.pop();
-    const inputs = inputList.map(input => input.value);
-    let email = inputs[0];
-    let password = inputs[1];
-    auth.signInWithEmailAndPassword(email, password).then(() => {
-      getUserFromDb(email).then(() => render(state.Home));
-      router.navigate("/Home");
-    });
-  });
-}
-function getUserFromDb(email) {
-  return db
-    .collection("Users")
-    .get()
-    .then(snapshot =>
-      snapshot.docs.forEach(doc => {
-        if (email === doc.data().email) {
-          let id = doc.id;
-          db.collection("Users")
-            .doc(id)
-            .update({ signedIn: true });
-          let user = doc.data();
-          state.User.username = user.firstName + user.lastName;
-          state.User.firstName = user.firstName;
-          state.User.lastName = user.lastName;
-          state.User.email = email;
-          state.User.loggedIn = true;
-        }
-      })
-    );
 }
